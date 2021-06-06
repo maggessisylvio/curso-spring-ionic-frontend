8222f43e2776466446e3fb5335b5e90a56bb271d
Sylvio Maggessi <sylviodev@gmail.com>
2021-06-06 17:37:13 -0300

    listando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { isThisTypeNode } from 'typescript';
+import { CategoriaService } from '../../services/domain/categoria.service';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -15,11 +17,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public categoriaService: CategoriaService
+  ) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+    this.categoriaService.findAll().subscribe(response => {
+      console.log(response);
+    },
+      error => {
+        console.log(error);
+      });
   }
 
 }
