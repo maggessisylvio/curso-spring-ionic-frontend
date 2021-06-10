dc454436d37ad7bdf5686b898320d7fd471c282c
Sylvio Maggessi <sylviodev@gmail.com>
2021-06-09 22:48:12 -0300

    carregando as imagens dos produtos

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, LoadingController, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 
@@ -24,8 +25,18 @@ export class ProdutosPage {
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       },
         error => { });
   }
 
+  loadImageUrls() {
+    this.items.forEach(produto => {
+      this.produtoService.getSmallImageFromBucket(produto.id)
+        .subscribe(response => {
+          produto.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${produto.id}-small.jpg`
+        },
+          error => { });
+    });
+  }
 }
